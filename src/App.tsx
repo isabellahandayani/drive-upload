f0ab14423663f58a367cee9a10108a8919ca75f7
isabellahandayani <isabellahandayani@gmail.com>
2022-03-20 18:12:13 +0700

    add resumeable upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [value, setValue] = useState<any>();
+  const [progress, setProgress] = useState<number>();
+
   const handleChange = (e: any) => {
     if (e.target.files) {
       setValue(e.target.files[0]);
@@ -9,40 +11,50 @@ function App() {
   };
 
   const handleClick = () => {
-    let file = new Blob([value], { type: value.type });
-    let metadata = {
-      name: value.name, // Filename at Google Drive
-      mimeType: value.type, // mimeType at Google Drive
-      parents: [import.meta.env.VITE_FOLDER], // Folder ID at Google Drive
-    };
-
-    let form = new FormData();
-    form.append(
-      "metadata",
-      new Blob([JSON.stringify(metadata)], { type: "application/json" })
-    );
-    form.append("file", file);
-
-    fetch(
-      "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=webViewLink,id",
-      {
-        method: "POST",
-        headers: new Headers({ Authorization: "Bearer " + import.meta.env.VITE_ACCESS_TOKEN }),
-        body: form,
-      }
-    )
-      .then((res) => {
-        return res.json();
-      })
-      .then(function (val) {
-        console.log(val);
-      });
+    if (value) {
+      let resource = {
+        file: value,
+        accessToken: import.meta.env.VITE_ACCESS_TOKEN,
+        folderId: import.meta.env.VITE_FOLDER,
+      };
+      resumeableUpload(resource);
+    }
   };
+
+  const resumeableUpload = (resource: any) => {
+    const ru = new ResumableUploadToGoogleDrive2();
+    ru.Do(resource, function (res: any, err: any) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      if (res.status == "Uploading") {
+        let msg = Math.round(
+          (res.progressNumber.current / res.progressNumber.end) * 100
+        );
+        setProgress(msg);
+      }
+    });
+  };
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "https://cdn.jsdelivr.net/gh/tanaikech/ResumableUploadForGoogleDrive_js@master/resumableupload2_js.min.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
   return (
     <div className="App">
       <input type="file" id="files" name="file" onChange={handleChange}></input>
       <button onClick={handleClick}>Submit</button>
+      {progress && <div>{progress}</div>}
     </div>
   );
 }
